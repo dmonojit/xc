ba253dc70214e32eafa9769a0746ba7cbc755fa4
Monojit Dey <714dmonojit@gmail.com>
2018-05-23 10:22:05 +0530

    some buggy file parsing code

---
+++ utils/util.py
@@ -0,0 +1,39 @@
+from scipy.sparse import *
+from scipy import *
+
+def get_matrices_from_file(filepath):
+	with open(filepath) as file:
+		content = file.read().splitlines()
+
+	total_points, feature_dm, number_of_labels = map(lambda x: int(x), content.pop(0).split(' '))
+
+	feature_indptr = [0]
+	feature_indices = []
+	feature_data = []
+	
+	label_vectors = []
+
+	for row in content:
+		row = row.split(' ')
+		labels = []
+
+		if row[0] == '':
+			label_vectors.append([])
+		else:
+			label_vectors.append([*map(lambda x: int(x), row.pop(0).split(','))])
+
+		length = len(row)
+		for datapoint in row:
+			index, value = datapoint.split(':')
+			index = int(index)
+			value = double(value)
+			feature_indices.append(index)
+			feature_data.append(value)
+		feature_indptr.append(length)
+
+	feature_matrix = csr_matrix((feature_data, feature_indices, feature_indptr), dtype=double)
+
+	return total_points, feature_dm, number_of_labels, feature_matrix, label_vectors
+
+if __name__ == '__main__':
+	print(123)
